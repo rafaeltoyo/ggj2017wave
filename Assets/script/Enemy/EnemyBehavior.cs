36479181edbab53824dbe9cfd045bac8e8cf79e0
rafaeltoyo <rafael_toyo@hotmail.com>
2017-01-21 21:51:48 -0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Assets/prefab/Bar.prefab
    #	Assets/script/EnemyBehavior.cs

---
+++ Assets/script/Enemy/EnemyBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CapsuleCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class EnemyBehavior : CharacterBase
+{
+
+    [SerializeField]
+    protected float velocidade;
+    [SerializeField]
+    protected int dano;
+    [SerializeField]
+    protected string[] TagsParaIgnorar;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        this.currentLife = this.maxLife;   
+    }
+
+    public bool validarTag(RaycastHit2D hit)
+    {
+        string tag = (hit.transform != null ? hit.collider.gameObject.tag : string.Empty);
+        foreach (string iTag in TagsParaIgnorar)
+        {
+            if (iTag.Equals(tag))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag.Equals("Player"))
+        {
+            PlayerBehavior playerBehavior = coll.gameObject.GetComponent<PlayerBehavior>();
+            playerBehavior.onDamage(dano);
+        }
+    }
+}

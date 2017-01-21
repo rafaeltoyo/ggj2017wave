35175cdd36b25217c2e8bb2ed197cb38b97bf445
rafaeltoyo <rafael_toyo@hotmail.com>
2017-01-21 08:02:15 -0200

    tiro unico funcional

---
+++ Assets/script/Projectile/ProjectileBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileBehaviour : MonoBehaviour {
+
+    [SerializeField]
+    [Range(100,1000)]
+    int projectileSpeed;
+
+    // Use this for initialization
+    void Start () {
+        //GetComponent<Rigidbody2D>().AddForce(new Vector2(projectileSpeed*transform.rotation.x, projectileSpeed*transform.rotation.y));
+        GetComponent<Rigidbody2D>().velocity.Set(10.0f, 10.0f);
+        Destroy(gameObject, 2);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(!coll.gameObject.tag.Equals("Projectile"))
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/script/Projectile/ShotWaveBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotWaveBehaviour : MonoBehaviour {
+
+    [SerializeField]
+    GameObject projectile;
+
+    // Use this for initialization
+    void Start () {
+        Instantiate(projectile, transform.position, Quaternion.AngleAxis(30, transform.rotation.eulerAngles));
+        Destroy(gameObject, 2);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

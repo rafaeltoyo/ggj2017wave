5d3d763e2b0a7cbc19b913588dae879aa1b95323
dioguima <diogoferreiraguima@hotmail.com>
2017-01-21 07:17:32 -0200

    Atualização do script do inimigo e criação do menu

---
+++ Assets/script/GameController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instancia;
+
+    public void Start()
+    {
+        instancia = this;
+    }
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) && !SceneManager.GetActiveScene().name.Equals("Menu"))
+        {
+            pausarJogo();
+        }
+    }
+
+    public void clickNovoJogo()
+    {
+        SceneManager.LoadScene("TesteDiogo");
+    }
+
+    public void clickSair()
+    {
+        Application.Quit();
+    }
+
+    public void pausarJogo()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void voltarJogo()
+    {
+        Time.timeScale = 1;
+    }
+}

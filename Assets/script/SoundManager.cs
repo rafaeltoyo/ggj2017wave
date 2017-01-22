ac97544e61f76c2b4414b7a32cbb803ab3eaaba8
rafaeltoyo <rafael_toyo@hotmail.com>
2017-01-22 12:57:29 -0200

    Fix Bug

--- Assets/script/SoundManager.cs
+++ Assets/script/SoundManager.cs
@@ -19,7 +19,7 @@ public class SoundManager : MonoBehaviour
         else if (instance != this)
             Destroy(gameObject);
 
-        DontDestroyOnLoad(gameObject);
+
     }
     
     public void PlaySingle(AudioClip clip)

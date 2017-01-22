ab6e79a3230cfd5be73997e587c5908064ed5e65
rafaeltoyo <rafael_toyo@hotmail.com>
2017-01-22 10:22:36 -0200

    Fase terminada

---
+++ Assets/script/BottomMapLimit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottomMapLimit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag.Equals("Player"))
+        {
+            PlayerBehavior playerBehavior = coll.gameObject.GetComponent<PlayerBehavior>();
+            playerBehavior.onDamage(100);
+        }
+    }
+}

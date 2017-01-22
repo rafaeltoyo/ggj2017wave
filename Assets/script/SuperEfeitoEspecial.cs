0ba260ebf207d6224b4cf385433640b59dbbf4ba
rafaeltoyo <rafael_toyo@hotmail.com>
2017-01-22 04:48:33 -0200

    Fase 1 - Plataformas

---
+++ Assets/script/SuperEfeitoEspecial.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuperEfeitoEspecial : MonoBehaviour {
+
+    [SerializeField]
+    [Range(1,10)]
+    float luminosidade;
+    [SerializeField]
+    [Range(0.1f, 1f)]
+    float desvioLuminosidade;
+    [SerializeField]
+    [Range(0.05f,0.1f)]
+    float desvioPosicao;
+
+    Vector3 posicaoPadrao;
+
+	// Use this for initialization
+	void Start () {
+        posicaoPadrao = transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.position = new Vector3(posicaoPadrao.x+Random.Range(-desvioPosicao, desvioPosicao), posicaoPadrao.y+Random.Range(-desvioPosicao, desvioPosicao));
+        GetComponent<Light>().intensity = Random.Range(luminosidade, luminosidade+desvioLuminosidade);
+    }
+}
